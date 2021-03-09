fbf3f8f40322f139d2365ab8d4888e6490814688
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-03-09 21:42:16 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,16 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+from report_vitals import report_battery_vitals
+from filter_values import filterOut_safe_vitals             
 
-  return True
+def is_battery_ok(bms_attributes):
+    value = dict(filter(filterOut_safe_vitals,bms_attributes.items()))
+    report_battery_state(bms_attributes)
+    return len(value) == 0
 
-
+def report_battery_state(bms_attributes):    
+    report_battery_vitals(bms_attributes)
+    
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(is_battery_ok({'temperature': 25,'Soc': 70, 'Charge_rate': 0.7}) is True)  #all values in limit
+  assert(is_battery_ok({'Temperature': 43,'soc': 24, 'Charge_rate': 0.5}) is False)  #high temp warning,low soc warning
+  assert(is_battery_ok({'Temperature': 25,'Soc': 60, 'charge_rate': 0.03}) is False) #low charge warning  
