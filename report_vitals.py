6aaed98d5c448aab6272e0ce7216fe948a57ef90
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-03-10 10:29:30 +0530

    Update report_vitals.py

--- report_vitals.py
+++ report_vitals.py
@@ -12,13 +12,15 @@ def report_normal_vitals():
     print(Get_Lang_Based_Word("Battery is ok",lang,lang_message_list))
     print("\n")
     
-def report_abnormal_vitals(abnormal_vitals,range_limits):   
-   messages =[]   
+def report_abnormal_vitals(abnormal_vitals,range_limits):
+   
+   messages =[]
+   
    for i in abnormal_vitals.items():
-      limitRanges = range_limits[i[0].upper()]
-      message = abnormal_attribute_and_limt(limitRanges,i[1])
-      if not(message is None):
-          messages.append(message)           
+       limitRanges = range_limits[i[0].upper()]
+       message = abnormal_attribute_and_limt(limitRanges,i[1])
+       if not(message is None):
+           messages.append(message)
    print_abnormal_vitals(messages)
 
 def print_abnormal_vitals(messages):
@@ -30,17 +32,21 @@ def print_abnormal_vitals(messages):
         
 def abnormal_attribute_and_limt(ranges,attributeValue):
     count = 0
-    message = ""
     valueStringByLang = Get_Lang_Based_Word("Value",lang,lang_message_list)
+    message = ""
     for rangeVal in ranges:
-        message = get_message_to_print(rangeVal,valueStringByLang,attributeValue,count)
-        if (count != 2): #Normal range, so no need to inform user
-            return message
+        tempMessage = get_message_to_print(rangeVal,valueStringByLang,attributeValue,count)
+        message = validate_range_message(tempMessage,count)
         count+=1
-    return None
+    return message
 
+def validate_range_message(message,count):
+    if (count != 2) and not(message is None): #Normal range, so no need to inform user
+        return message
+    
 def get_message_to_print(rangeAndVal, valueStrByLang,attributeValue,count):
     if count==4:
         return rangeAndVal+" | "+valueStrByLang+" : "+str(attributeValue)
     elif attributeValue<=rangeAndVal[0]:
-        return rangeAndVal[1] +" | "+valueStrByLang+" : "+str(attributeValue)    
+        return rangeAndVal[1] +" | "+valueStrByLang+" : "+str(attributeValue)
+ 
