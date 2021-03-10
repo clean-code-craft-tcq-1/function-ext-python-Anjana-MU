5a001896d4d0150fa8c3f7cd39fcd8de2c70027f
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-03-10 09:58:59 +0530

    Update report_vitals.py

--- report_vitals.py
+++ report_vitals.py
@@ -12,29 +12,35 @@ def report_normal_vitals():
     print(Get_Lang_Based_Word("Battery is ok",lang,lang_message_list))
     print("\n")
     
-def report_abnormal_vitals(abnormal_vitals,range_limits):
-   print(Get_Lang_Based_Word("Abnormal Vitals",lang,lang_message_list))
-    
+def report_abnormal_vitals(abnormal_vitals,range_limits):   
+   messages =[]   
    for i in abnormal_vitals.items():
-       limitRanges = range_limits[i[0].upper()]
-       print(abnormal_attribute_and_limt(limitRanges,i[1]))
-   print("\n")
+      limitRanges = range_limits[i[0].upper()]
+      message = abnormal_attribute_and_limt(limitRanges,i[1])
+      if not(message is None):
+          messages.append(message)           
+   print_abnormal_vitals(messages)
 
+def print_abnormal_vitals(messages):
+    if len(messages)>0:
+        print(Get_Lang_Based_Word("Abnormal Vitals",lang,lang_message_list))
+        for message in messages:
+            print(message)
+        print("\n")
+        
 def abnormal_attribute_and_limt(ranges,attributeValue):
     count = 0
     message = ""
-    attrValAsString = str(attributeValue)
     valueStringByLang = Get_Lang_Based_Word("Value",lang,lang_message_list)
     for rangeVal in ranges:
-        if count == 4: #If the last range is reached, then it means the attribute has exceeded all limits
-           message = rangeVal + " | "+valueStringByLang+" : "+attrValAsString #the last item in limitranges is a single item, hence index is not neede for rangeVal
-           break
-        elif attributeValue<=rangeVal[0]:
-            if count == 2: #If the range is in safe region, then the battery data doesn't need to be published
-               break
-            message = rangeVal[1] +" | "+valueStringByLang+" : "+attrValAsString
-            break
-            
+        message = get_message_to_print(rangeVal,valueStringByLang,attributeValue,count)
+        if (count != 2) and not(message is None): #Normal range, so no need to inform user
+            return message
         count+=1
-    return message
-    
+    return None
+
+def get_message_to_print(rangeAndVal, valueStrByLang,attributeValue,count):
+    if count==4:
+        return rangeAndVal+" | "+valueStrByLang+" : "+str(attributeValue)
+    elif attributeValue<=rangeAndVal[0]:
+        return rangeAndVal[1] +" | "+valueStrByLang+" : "+str(attributeValue)    
