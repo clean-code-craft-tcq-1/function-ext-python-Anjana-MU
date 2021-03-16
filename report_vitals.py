d568c41e1254262daa8816431373a3195d5a82c6
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-03-16 21:19:49 +0530

    Update report_vitals.py

--- report_vitals.py
+++ report_vitals.py
@@ -1,10 +1,10 @@
 from filter_values import filterOut_safe_vitals
-from battery_limits import Get_Lang_Based_Word, bms_range_limits, lang,lang_message_list
+from battery_limits import Get_Lang_Based_Word, lang, lang_message_list
 
-def report_battery_vitals(bms_attributes):
-    abnormal_vitals = dict(filter(filterOut_safe_vitals,bms_attributes.items()))
+def report_battery_vitals(processed_data):
+    abnormal_vitals = list(filter(filterOut_safe_vitals,processed_data))
     if len(abnormal_vitals) > 0:
-        report_abnormal_vitals(abnormal_vitals,bms_range_limits)
+        report_abnormal_vitals(abnormal_vitals)
     else:
         report_normal_vitals()
         
@@ -12,41 +12,32 @@ def report_normal_vitals():
     print(Get_Lang_Based_Word("Battery is ok",lang,lang_message_list))
     print("\n")
     
-def report_abnormal_vitals(abnormal_vitals,range_limits):
-   
-   messages =[]
-   
-   for i in abnormal_vitals.items():
-       limitRanges = range_limits[i[0].upper()]
-       message = abnormal_attribute_and_limt(limitRanges,i[1])
-       if not(message is None):
-           messages.append(message)
-   print_abnormal_vitals(messages)
+def report_abnormal_vitals(abnormal_vitals):   
+    messages =[]
+    messages.extend(get_abnormal_values_heading())
+    for i in abnormal_vitals:
+        message = '{:>20}'.format(str(i[1])+"   |   ")
+        message+='{:>30}'.format(str(i[3])+"   |   ")
+        message+='{:>12}'.format("{:.2f}".format(i[2])+"   |   ")
+        message+='{:>12}'.format("{:.2f}".format(i[4][1])+" - "+"{:.2f}".format(i[4][2]))
+        messages.append(message)
+    print_abnormal_vitals(messages)
 
-def print_abnormal_vitals(messages):
-    if len(messages)>0:
-        print(Get_Lang_Based_Word("Abnormal Vitals",lang,lang_message_list))
-        for message in messages:
-            print(message)
-        print("\n")
-        
-def abnormal_attribute_and_limt(ranges,attributeValue):
-    count = 0
+def get_abnormal_values_heading():
     valueStringByLang = Get_Lang_Based_Word("Value",lang,lang_message_list)
-    message = ""
-    for rangeVal in ranges:
-        tempMessage = get_message_to_print(rangeVal,valueStringByLang,attributeValue,count)
-        message = validate_range_message(tempMessage,count)
-        count+=1
-    return message
-
-def validate_range_message(message,count):
-    if (count != 2) and not(message is None): #Normal range, so no need to inform user
-        return message
+    normalRangeStringbyLang = Get_Lang_Based_Word("Normal range",lang,lang_message_list)
+    attributeStringByLang = Get_Lang_Based_Word("Attribute",lang,lang_message_list)
+    infoStringByLang = Get_Lang_Based_Word("Info",lang,lang_message_list)    
+    line1 = '{:>20}'.format(attributeStringByLang+"   |   ")
+    line1+='{:>30}'.format(infoStringByLang+"   |   ")
+    line1+='{:>12}'.format(valueStringByLang+"   |   ")
+    line1+='{:>12}'.format(normalRangeStringbyLang)    
+    line2 ="-"*80
+    return (line1, line2)
     
-def get_message_to_print(rangeAndVal, valueStrByLang,attributeValue,count):
-    if count==4:
-        return rangeAndVal+" | "+valueStrByLang+" : "+str(attributeValue)
-    elif attributeValue<=rangeAndVal[0]:
-        return rangeAndVal[1] +" | "+valueStrByLang+" : "+str(attributeValue)
- 
+def print_abnormal_vitals(messages):
+    print(Get_Lang_Based_Word("Abnormal Vitals",lang,lang_message_list))
+    print("\n")
+    for message in messages:
+        print(message)
+    print("\n")      
