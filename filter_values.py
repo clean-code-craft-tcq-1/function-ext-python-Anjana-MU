bc47fdf89a60892c8cd7758650f32eeefff6f748
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-03-16 21:18:46 +0530

    Update filter_values.py

--- filter_values.py
+++ filter_values.py
@@ -1,6 +1,2 @@
-from battery_limits import bms_range_limits
-
-def filterOut_safe_vitals(item):
-    low_warning_val = bms_range_limits[item[0].upper()][1][0] 
-    high_warning_val = bms_range_limits[item[0].upper()][2][0]   
-    return (item[1] <= low_warning_val) or (item[1] >= high_warning_val)
+def filterOut_safe_vitals(item):   
+    return ( "_" in item[3]) # except normal all other string will contain '_'
